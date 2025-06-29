91e15f59039d3d01daab13ef28f72121cc39d294
martinmaged72 <148314002+martinmaged72@users.noreply.github.com>
2025-06-29 15:27:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// button clicked
+let buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+    let buttonInnerHtml = this.innerHTML;
+    let currentValue = document.querySelector("#field").value;
+
+    switch (buttonInnerHtml) {
+        case "Ac":
+        document.querySelector("#field").value = "";
+        break;
+
+        case "Del":
+        document.querySelector("#field").value = currentValue.slice(0, -1);
+        break;
+
+        case "=":
+        document.querySelector("#field").value = eval(currentValue);
+        break;
+
+        default:
+        document.querySelector("#field").value = currentValue + buttonInnerHtml;
+    }
+    });
+}
